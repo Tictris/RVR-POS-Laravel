b5b6ba75c445ad1cf48c3724b41466529cb655e4
Yousef <72538140+Tictris@users.noreply.github.com>
2024-10-08 14:31:42 +0800

    Revert "cottages and customers crud function"

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,34 +12,8 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $data = [
-            
-            "name"=> "jason",
-            "total"=>   1000,
-            "bc" => [
-                [
-                    "cottage_id" => 1,
-                    "quantity"  => 1
-                ],
-                [
-                    "cottage_id" => 2,
-                    "quantity"  => 1
-                ]
-                ],
-                "cc"  => [
-                    [
-                        "customer_id" => 1,
-                        "count" => 5
-                    ],
-                    [
-                        "customer_id" => 2,
-                        "count" => 5
-                    ]
-                ]
-    ];
+        $response = $this->get('/');
 
-    $response = $this->post('/api/create-entrance', $data);
-    $response->assetCreated();
-    $this->assertDatabaseHas('entrances', $data);
+        $response->assertStatus(200);
     }
 }
